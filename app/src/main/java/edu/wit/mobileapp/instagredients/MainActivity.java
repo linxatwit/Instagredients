3783487fb34fb2d1b2691888c11bbece14b1ce10
Xi Xi Lin <42873025+linxatwit@users.noreply.github.com>
2021-06-24 16:05:46 -0400

    Nav bar

--- app/src/main/java/edu/wit/mobileapp/instagredients/MainActivity.java
+++ app/src/main/java/edu/wit/mobileapp/instagredients/MainActivity.java
@@ -1,14 +1,65 @@
 package edu.wit.mobileapp.instagredients;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
+    private NavigationView nv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dl = (DrawerLayout)findViewById(R.id.activity_main);
+        t = new ActionBarDrawerToggle(this, dl,R.string.Open, R.string.Close);
+
+        dl.addDrawerListener(t);
+        t.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        nv = (NavigationView)findViewById(R.id.nv);
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch(id)
+                {
+                    case R.id.home:
+                        Toast.makeText(MainActivity.this, "Home",Toast.LENGTH_SHORT).show();
+                    case R.id.saved:
+                        Toast.makeText(MainActivity.this, "Saved",Toast.LENGTH_SHORT).show();
+                    default:
+                        return true;
+                }
+
+
+
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(t.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
     }
 }
