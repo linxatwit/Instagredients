0bed3ba2c10941e2c44dc0ba8191e5803fbdd63b
Xi Xi Lin <42873025+linxatwit@users.noreply.github.com>
2021-07-31 19:01:53 -0400

    Revert "updating csv"
    This reverts commit b02ff69072878fdcf9596880a51844a07f40eb24.

--- ingredients.py
+++ ingredients.py
@@ -3,7 +3,7 @@ import pandas as pd
 import csv
 
 # list of ingredients from Supercook
-list = ['pepper', 'beans', ':', 'batter', 'cranberries', 'olive oil', 'vegetable oil','shallot', 'spray', 'water', 'toppings', 'sauce', 'spice', 'mint', 'dough', 'butter', 'seed', 'basil', 'oregano', 'thyme', 'chocolate', 'vanilla', 'baking powder', 'butter milk', 'cocoa', 'cheese', 'custard', 'egg', 'ice cream', 'milk', 'sour cream', 'yogurt', 'apple', 'banana', 'blackberries', 'blueberries', 'cherries', 'coconut', 'cranberries', 'salt', 'grapes', 'kiwi', 'lemon', 'lime', 'mango', 'orange', 'peach', 'pear', 'pineapple', 'raspberries', 'strawberries', 'baking soda', 'bread', 'flour', 'noodles', 'pasta', 'rice', 'yeast', 'beef soup', 'chicken soup', 'beer', 'lamb soup', 'oil', 'red wine', 'white wine', 'bacon', 'beef', 'chicken', 'ham', 'lamb', 'pork', 'salami', 'sausage', 'turkey', 'almond', 'cashew', 'macadamia', 'peanut', 'peanut butter', 'walnut', 'carp', 'catfish', 'crab', 'eel', 'lobster', 'mackerel', 'mussel', 'oyster', 'prawn', 'salmon', 'sardine', 'scallop', 'shrimp', 'squid', 'trout', 'tuna', 'barbeque sauce', 'syrup', 'fish sauce', 'honey', 'mayonnaise', 'mustard', 'soy sauce', 'vinegar', 'sugar', 'avocado', 'broccoli', 'cabbage', 'carrot', 'cauliflower', 'celery', 'corn', 'cucumber', 'eggplant', 'garlic', 'ginger', 'lettuce', 'mushroom', 'olive', 'onion', 'pickle', 'potato', 'pumpkin', 'spinach', 'sweet potato', 'tomato', 'zucchini', 'asparagus']
+list = ['pepper', 'beans', ':', 'batter', 'cranberries', 'olive oil', 'vegetable oil','shallot', 'spray', 'water', 'toppings', 'sauce', 'spice', 'mint', 'dough', 'butter', 'seed', 'basil', 'oregano', 'thyme', 'chocolate', 'vanilla', 'baking powder', 'butter milk', 'cocoa', 'cheese', 'custard', 'egg', 'ice cream', 'milk', 'sour cream', 'yogurt', 'apple', 'banana', 'blackberries', 'blueberries', 'cherries', 'coconut', 'cranberries', 'salt', 'grapes', 'kiwi', 'lemon', 'lime', 'mango', 'orange', 'peach', 'pear', 'pineapple', 'raspberries', 'strawberries', 'baking soda', 'bread', 'flour', 'noodles', 'pasta', 'rice', 'yeast', 'beef soup', 'chicken soup', 'beer', 'lamb soup', 'oil', 'red wine', 'white wine', 'bacon', 'beef', 'chicken', 'ham', 'lamb', 'pork', 'salami', 'sausage', 'turkey', 'almond', 'cashew', 'macadamia', 'peanut', 'peanut butter', 'walnut', 'carp', 'catfish', 'crab', 'eel', 'lobster', 'mackerel', 'mussel', 'oyster', 'prawn', 'salmon', 'sardine', 'scallop', 'shrimp', 'squid', 'trout', 'tuna', 'barbeque sauce', 'syrup', 'fish sauce', 'honey', 'mayonnaise', 'mustard', 'soy sauce', 'vinegar', 'sugar', 'avocado', 'broccoli', 'cabbage', 'carrot', 'cauliflower', 'celery', 'corn', 'cucumber', 'eggplant', 'garlic', 'ginger', 'lettuce', 'mushroom', 'olive', 'onion', 'pickle', 'potato', 'pumpkin', 'spinach', 'sweet potato', 'tomato', 'zucchini']
 
 import sys
 sys.setrecursionlimit(6600)
@@ -20,8 +20,8 @@ contain = []
 
 with open('recipes.csv','a', newline='') as fd:
     for recipe in data[:325]:
-        print("https:" + recipe)
-        scraper = scrape_me("https:" + recipe)
+        print(recipe)
+        scraper = scrape_me(recipe)
         ingredients = scraper.ingredients()
         title = scraper.title()
         #print(title)
